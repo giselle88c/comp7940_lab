c0fd85d49d76a120c32fd08c7d496d91808d3baa
giselle88c <cotton00@gmail.com>
2025-01-23 12:10:33 +0800

    ex

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 def main():
     print("hello world")
 
+def Ex1(factor):
+    x=factor
+
+    for i in range(x+1):
+        if(i!=0):
+            if x%i==0:
+                print(i,end=" ")
+    print()
+
+
 if __name__ == '__main__':
     main()
+    l = [52633, 8137, 1024, 999]
+    for fact in l:
+        Ex1(fact)
