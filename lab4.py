f2dc3fdf47c3956d6779f7acd65dde8538109f5f
giselle88c <cotton00@gmail.com>
2025-02-22 21:23:53 +0800

    modify chatgpt

---
+++ lab4.py
@@ -0,0 +1,20 @@
+"""Basic connection example.
+"""
+
+import redis
+
+r = redis.Redis(
+    host='redis-19958.c256.us-east-1-2.ec2.redns.redis-cloud.com',
+    port=19958,
+    decode_responses=True,
+    username="default",
+    password="bxPZDk4Kq7oAa92lP3ht8efkvgYQr5xD",
+)
+
+success = r.set('foo', 'bar')
+# True
+
+result = r.get('foo')
+print(result)
+# >>> bar
+
