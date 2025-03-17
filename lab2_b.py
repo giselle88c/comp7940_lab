593ce86383cac9fea1a6b4ae5918264a4cf5f65a
giselle88c <cotton00@gmail.com>
2025-03-17 21:07:30 +0800

    add folder

--- lab2_b.py
+++
@@ -1,8 +0,0 @@
-#Write a Python function that can reverse a string.
-
-def reverse_string(myText):
-  newText="".join(list(myText)[::-1])
-
-  return newText
-
-print(reverse_string("Hello, my name is ..."))
